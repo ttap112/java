bbfbf17a480334a844595479dfd9f18aeee108ab
Mirage <86884419+ttap112@users.noreply.github.com>
2023-07-12 16:44:42 +0900

    1~20까지의 정수 중 3의 배수의 합을 구하는 코드

--- Main.java
+++ Main.java
@@ -1,21 +1,17 @@
-package Main;
-
-import java.math.BigInteger;
-import java.util.Scanner;
+package M;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner Sc = new Scanner(System.in);
 		
-		BigInteger i = Sc.nextBigInteger();
-		BigInteger j = Sc.nextBigInteger();
+		int total=0;
 		
+		for (int i =1; i<=20; i++) {
+			System.out.println(i+" : "+total);
+			
+			if (i%3==0) total+=i;
+		}
+	
 		
-		System.out.println(i.divide(j));
-		System.out.println(i.remainder(j));
-		
-
 	}
-
 }
