fe68ae99b94b1cae0f04f4d78f40b0392b4dc98d
Mirage <86884419+ttap112@users.noreply.github.com>
2023-07-12 18:01:13 +0900

    각 인원의 총 점수와 평균을 구하는 코드

--- Main.java
+++ Main.java
@@ -1,33 +1,31 @@
 package M;
 
-import java.util.Random;
+
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	Random random = new Random();
 	
-	int row = sc.nextInt();
-	int col = sc.nextInt();
-	
-	int[] arr= new int[row*col];
-	
-	for (int i =0; i<arr.length; i++) {
-		arr[i] = random.nextInt(row*col)+1;
-		for (int j=0; j<i; j++) {
-			if(arr[i]==arr[j]) {
-				i--;
-				}
-			}
+		Scanner sc = new Scanner(System.in);
+		
+		int total=0;
+		int avg=0;
+		
+		System.out.print("인원을 입력 하시오 : ");
+		int m = sc.nextInt();
+		
+		for (int i=1; i<=m; i++) {
+			System.out.print("점수를 입력하시오 : ");
+			int n = sc.nextInt();
+			
+			total+=n;
 		}
-	for (int i=0; i<arr.length; i++) {
-		System.out.printf("%5d",arr[i]);
-		if (i!=0 && (i+1)%col==0) {
-			System.out.println();
-			}
-		}
+		avg= total/m;
+		
+		System.out.printf("총 %d명 의 총점은 %d 이고 평균은 %f 이다",m,total,(float)avg);
+	
+	
 	}
 }
 
