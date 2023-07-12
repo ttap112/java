36f0b0b705db814255dcd78eb35870f0487af2ab
Mirage <86884419+ttap112@users.noreply.github.com>
2023-07-12 17:47:41 +0900

    랜덤 자리 뽑기

--- Main.java
+++ Main.java
@@ -1,17 +1,33 @@
 package M;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		int total=0;
-		
-		for (int i =1; i<=20; i++) {
-			System.out.println(i+" : "+total);
-			
-			if (i%3==0) total+=i;
-		}
+	Scanner sc = new Scanner(System.in);
+	Random random = new Random();
 	
-		
+	int row = sc.nextInt();
+	int col = sc.nextInt();
+	
+	int[] arr= new int[row*col];
+	
+	for (int i =0; i<arr.length; i++) {
+		arr[i] = random.nextInt(row*col)+1;
+		for (int j=0; j<i; j++) {
+			if(arr[i]==arr[j]) {
+				i--;
+				}
+			}
+		}
+	for (int i=0; i<arr.length; i++) {
+		System.out.printf("%5d",arr[i]);
+		if (i!=0 && (i+1)%col==0) {
+			System.out.println();
+			}
+		}
 	}
 }
+
